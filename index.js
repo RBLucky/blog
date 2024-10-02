823427f243af132e40cef1db9e931ec143a6ceae
RBLucky <nkosi.s.lucky@gmail.com>
2024-10-02 14:53:36 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// Import modules
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+
+// Initialize express
+const app = new express();
+
+// Set templating engine
+app.set('view engine', 'ejs');
+
+// Serves static files from public folder
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact')
+})
+
+app.get('/post', (req, res) => {
+    res.render('post')
+})
+// Set port to listen on
+app.listen(4000, () => {
+    console.log('App listening on port 4000')
+})
