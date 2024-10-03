fb8c7d7092c582608ee26255109443130cf768ed
RBLucky <nkosi.s.lucky@gmail.com>
2024-10-03 12:02:51 +0200

    update blog repo

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const BlogPost = require('./models/BlogPost');
 
-mongoose.connect('mongodb://0.0.0.0:27017/blog_db', {useNewUrlParser: true});
+mongoose.connect('mongodb://0.0.0.0:27017/blog_db', { useNewUrlParser: true });
 
 // Initialize express
 const app = new express();
@@ -15,9 +16,14 @@ app.set('view engine', 'ejs');
 // Serves static files from public folder
 app.use(express.static('public'));
 
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded())
+
 // Route handling for each route
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+    const blogposts = await BlogPost.find({});
+    res.render('index', {blogposts});
 })
 
 app.get('/about', (req, res) => {
@@ -35,6 +41,12 @@ app.get('/post', (req, res) => {
 app.get('/posts/new', (req, res) => {
     res.render('create')
 })
+
+app.post('/posts/store', async (req, res) => {
+    await BlogPost.create(req.body)
+    res.redirect('/');
+})
+
 // Set port to listen on
 app.listen(4000, () => {
     console.log('App listening on port 4000')
