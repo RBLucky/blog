56709c58aa2796cb2aa9cec9b7eb79fd5311121b
RBLucky <nkosi.s.lucky@gmail.com>
2024-10-07 11:40:10 +0200

    implementing password encryption

--- models/User.js
+++ models/User.js
@@ -1,11 +1,20 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
     username: String,
     password: String
 });
+
+UserSchema.pre('save', function(next) {
+    const user = this
+    bcrypt.hash(user.password, 10, (error, hash) => {
+        user.password = hash;
+        next();
+    })
+})
 
 // Export Model
 const User = mongoose.model('User', UserSchema);
