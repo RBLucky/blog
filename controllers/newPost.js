807d0bda646d92f1fbe666712e3ece67d434dcf6
RBLucky <nkosi.s.lucky@gmail.com>
2024-10-08 11:33:36 +0200

    implementing user auth w/ express sessions

--- controllers/newPost.js
+++ controllers/newPost.js
@@ -2,9 +2,9 @@ module.exports = (req, res) => {
     if (req.session.userId) {
         // New post form
         res.render('create');        // Create 
+    } else {
+        // Renders login page if user
+        // attempts to make a post
+        res.redirect('/auth/login');
     }
-
-    // Renders login page if user
-    // attempts to make a post
-    res.redirect('/auth/login');
 };
