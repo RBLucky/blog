2de2f3abf3a6201d2417bb1b60d2c2bc14eff01e
RBLucky <nkosi.s.lucky@gmail.com>
2024-10-08 11:10:47 +0200

    implementing user auth w/ express sessions

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,10 +2,15 @@
 const User = require('../models/User');
 
 module.exports = (req, res, next) => {
-    User.findById(req.session.userId, (error, user) => {
-        if (error || !user)
-            return res.redirect('/')
-
-        next()
+    User.findById(req.session.userId)
+    .then((user) => {
+        if (!user) {
+            res.redirect('/');
+        }
+        next();
+    })
+    .catch((error) => {
+        res.redirect('/');
+        next();
     })
 }
