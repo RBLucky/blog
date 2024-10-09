45a8d2bbcb5cfa43a6b166c5d937b613240a4046
RBLucky <nkosi.s.lucky@gmail.com>
2024-10-09 11:02:54 +0200

    duplicate entry error implementation

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt');
+var uniqueValidator = require('mongoose-unique-validator');
 
 const UserSchema = new Schema({
     username: {
@@ -14,6 +15,8 @@ const UserSchema = new Schema({
         required: true
     }
 });
+
+UserSchema.plugin(uniqueValidator);
 
 UserSchema.pre('save', function(next) {
     const user = this
