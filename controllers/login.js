2e38001ca08e05437821ec1c4bb778de8548f5e1
RBLucky <nkosi.s.lucky@gmail.com>
2024-10-10 10:17:24 +0200

    make request data on form persist

--- controllers/login.js
+++ controllers/login.js
@@ -1,4 +1,19 @@
 
 module.exports = (req, res) => {
-    res.render('login')
-}
+    let username = "";
+    let password = "";
+    const details = req.flash('data')[0];
+
+    //data persists
+    if (typeof details != "undefined") {
+        username = details.username
+        password = details.password
+    }
+
+    res.render('login', {
+        errors: req.flash('loginError'),
+        username: username,
+        password: password
+    }) //login.ejs
+    console.log(details);
+};
