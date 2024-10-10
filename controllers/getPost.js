8ecd4cf9c6c3639f0b954e58ca7f6d1b28d00611
RBLucky <nkosi.s.lucky@gmail.com>
2024-10-10 10:32:45 +0200

    make request data on form persist

--- controllers/getPost.js
+++ controllers/getPost.js
@@ -1,8 +1,10 @@
 const BlogPost = require('../models/BlogPost');
 
 module.exports = async (req, res) => {
-    const blogpost = await BlogPost.findById(req.params.id);
+    const blogpost = await BlogPost.findById(req.params.id).populate('userid');
     res.render('post', {
         blogpost
     })
 }
+
+// .populate('userid');
