1225cd9c2c037821dbdd4eeb748aa2811386cc96
RBLucky <nkosi.s.lucky@gmail.com>
2024-10-11 10:12:26 +0200

    make request data on form persist

--- controllers/getPost.js
+++ controllers/getPost.js
@@ -1,10 +1,9 @@
+
 const BlogPost = require('../models/BlogPost');
 
 module.exports = async (req, res) => {
-    const blogpost = await BlogPost.findById(req.params.id).populate('userid');
+    const blogpost = await BlogPost.findById(req.params.id);
     res.render('post', {
         blogpost
-    })
-}
-
-// .populate('userid');
+    });
+};
--- controllers/storePost.js
+++ controllers/storePost.js
@@ -1,19 +1,11 @@
+
+const BlogPost = require('../models/BlogPost.js')
 const path = require('path');
-const BlogPost = require('../models/BlogPost');
 
 module.exports = (req, res) => {
     let image = req.files.image;
-    image.mv(path.resolve(__dirname, '..', 'public/assets/img', image.name),
-        async (error) => {
-            const validationErrors = Object.keys(error.errors).map(key =>
-                error.errors[key].message
-            );
-            req.flash('validationErrors', validationErrors);
-            req.flash('data', req.body)
-            res.redirect('/posts/new');
-            await BlogPost.create({
-                ...req.body,
-                image: "/assets/img/" + image.name
-            });
-        });
+    image.mv(path.resolve(__dirname, '..', 'public/assets/img', image.name))
+        .then(() => BlogPost.create({ ...req.body, image: '/assets/img/' + image.name, userid: req.session.userId }))
+        .then(blogpost => res.redirect('/'))
+        .catch(error => console.log(error))
 }
